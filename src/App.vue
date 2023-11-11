f196e1db01189100c660b723de1c0e42e746b6f4
Marcin Siwonia <mahinek12@gmail.com>
2023-11-11 16:00:42 +0100

    Init project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import todoApp from './components/todoApp.vue';
+</script>
+
+<template>
+  <todo-app />
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/todoApp.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TodoForm from '.todoForm.vue'
+import TodoList from '.todoList.vue'
+
+</script>
+
+<template>
+<div class="todo-app">
+    <h1>Todos App</h1>
+    <todo-form />
+    <todo-list />
+
+</div>
+</template>
+
+<style scoped>
+</style>
